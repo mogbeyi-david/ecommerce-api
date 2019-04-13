70d40c202dd3b6fa06b3281add6db14bb6a89e56
David Mogbeyi <mogbeyidavid@gmail.com>
2019-04-13 08:12:16 +0100

    Implemented the customer schema and model

--- models/customer.js
+++ models/customer.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
@@ -8,14 +8,68 @@ const customerSchema = new Schema({
     type: String,
     maxlength: 255
   },
+
   email: {
     required: true,
     type: String,
     maxlength: 255,
     unique: true
   },
+
   password: {
     required: true,
+    type: String,
+    minlength: 8,
+    maxlength: 255
+  },
+
+  creditCard: {
+    type: String
+  },
+
+  address_1: {
+    type: String
+  },
+
+  address_2: {
+    type: String
+  },
+
+  city: {
+    type: String
+  },
+
+  region: {
+    type: String
+  },
+
+  postalCode: {
+    type: String
+  },
+
+  country: {
+    type: String
+  },
+
+  shippingRegionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ShippingRegion'
+  },
+
+  dayPhoneNumber: {
+    type: String
+  },
+
+  nightPhoneNumber: {
+    type: String
+  },
+
+  mobilePhoneNumber: {
     type: String
   }
 });
+
+// Create the customer model from the customer Schema
+const Customer = mongoose.model('Customer', customerSchema);
+// Export the model
+export default Customer
