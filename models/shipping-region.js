64505ad9a764378761f36c04d719c472b0e2e59e
David Mogbeyi <mogbeyidavid@gmail.com>
2019-04-13 08:26:31 +0100

    Added comments to the shipping region model file

--- models/shipping-region.js
+++ models/shipping-region.js
@@ -10,6 +10,9 @@ const shippingRegionSchema = new Schema({
   }
 });
 
+
+//  Create the shipping region model from the schema
 const ShippingRegion = mongoose.model('ShippingRegion', shippingRegionSchema);
 
+//  Export the ShippingRegion model
 export default ShippingRegion;
