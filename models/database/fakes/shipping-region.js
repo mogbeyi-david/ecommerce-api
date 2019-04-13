9d3aada2a8881f765529c7cba8859b1c7176ac68
David Mogbeyi <mogbeyidavid@gmail.com>
2019-04-13 15:35:42 +0100

    implemented the shipping region seeder

---
+++ models/database/fakes/shipping-region.js
@@ -0,0 +1,31 @@
+import {ShippingRegion} from '../../../models';
+
+
+/**
+ *  Class responsible for generating fake shipping regions
+ */
+class ShippingRegionSeeder {
+
+  /**
+   *
+   * @returns {Promise<void>}
+   */
+  async generateFakeShippingRegion() {
+    const shippingRegions = ['Please Select', 'US / Canada', 'Europe', 'Rest of World'];
+    console.log('---------Generating 4 Fake Shipping Regions-------------');
+    try {
+      for (let count = 0; count < 3; count++) {
+        const shippingRegion = new ShippingRegion({
+          shippingRegion: shippingRegions[count]
+        });
+        await shippingRegion.save();
+      }
+      return console.log('---------Finished generating 4 Fake Shipping Regions-------------');
+    } catch (exception) {
+      return console.log()
+    }
+  }
+}
+
+const shippingRegionSeeder = new ShippingRegionSeeder();
+export default shippingRegionSeeder;
