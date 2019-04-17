c2b87424141fcd414e42d39a79b5e09b9cbc34c2
David Mogbeyi <mogbeyidavid@gmail.com>
2019-04-17 15:53:07 +0100

    Modified the creation of shipping region test case

--- test/integration/controllers/shipping-region-controller.test.js
+++ test/integration/controllers/shipping-region-controller.test.js
@@ -22,13 +22,12 @@ describe('Testing Endpoints for Shipping Regions', () => {
   describe('POST ENDPOINTS', () => {
     describe('Creating a New Shipping Region', () => {
       //  Testcase for when no shipping region was passed in the request payload
-      it('should return a 400 error if no shipping region was passed in the request payload', () => {
+      it('should return a 400 error if no shipping region was passed in the request payload', async () => {
         const payload = {}; // Declare the empty payload
-        const response = request(app).post(baseEndpoint).send(payload); // Send a post request to the endpoint
+        const response = await request(app).post(baseEndpoint).send(payload); // Send a post request to the endpoint
         expect(response).to.be.a('object');
         expect(response.status).to.equal(400);
-        expect(response.message).to.be.a('string');
-        expect(response.message).to.match(/Error/);
+        expect(response.body.message).to.be.a('string');
         expect(response.body).to.be.a('object');
       });
     });
