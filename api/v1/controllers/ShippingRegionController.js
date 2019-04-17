758e231a847a7f17fbadfb629ad4cf0fdbf6f02c
David Mogbeyi <mogbeyidavid@gmail.com>
2019-04-17 14:27:50 +0100

    Installed http-status-codes package

--- api/v1/controllers/ShippingRegionController.js
+++ api/v1/controllers/ShippingRegionController.js
@@ -1,6 +1,15 @@
 import ShippingRegion from '../../../models/shipping-region';
+import validateShippingRegion from '../../../validations';
+import * as HttpStatus from 'http-status-codes';
+
 
 class ShippingRegionController {
+  create(req, res) {
+    const {error, value} = validateShippingRegion(req.body);
+    if (error) {
+      return res.status(HttpStatus.BAD_REQUEST).send({message: error.details[0].message, data: value})
+    }
+  }
 
 }
 
