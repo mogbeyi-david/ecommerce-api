f998a8894b2ac2bc6adf443757b9d2691232bb90
David Mogbeyi <mogbeyidavid@gmail.com>
2019-04-22 22:41:27 +0100

    Implemented the auth middleware

---
+++ middlewares/admin.js
@@ -0,0 +1,10 @@
+import * as HttpStatus from 'http-status-codes';
+
+function admin(req, res, next) {
+  if (req.user.role !== 'ADMIN') {
+    return res.status(HttpStatus.FORBIDDEN).send({message: 'Access Denied', data: null});
+  }
+  next();
+}
+
+export default admin;
---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+import * as HttpStatus from 'http-status-codes';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import JsonResponse from '../helpers/response/json-response';
+
+function auth(req, res, next) {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return JsonResponse.error(res, HttpStatus.BAD_REQUEST, 'No token provided', null);
+  }
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    next();
+  } catch (exception) {
+    return res.status(HttpStatus.BAD_REQUEST).send({message: 'Invalid token', data: null});
+  }
+}
+
+export default auth;
